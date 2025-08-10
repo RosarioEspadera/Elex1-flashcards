3ae6ca93add91af57dc55498f2a0c9953fe0c782
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-10 18:51:18 +0000

    5

--- scripts-Electronics1/main.js
+++ scripts-Electronics1/main.js
@@ -10,11 +10,6 @@ async function loadTopic(topic) {
   renderFlashcards(cards); // Optional: show all cards in grid
 }
 
-const themeToggle = document.getElementById("theme-toggle");
-themeToggle.onclick = () => {
-  document.body.classList.toggle("dark");
-};
-
 const topicCards = document.querySelectorAll(".topic-card");
 const flashcardViewer = document.querySelector(".flashcard-viewer");
 const flashcard = document.querySelector(".flashcard");
