a6a23abc969bac19d6fdbe5613f8c89c066d2726
RosarioEspadera <s.rvespadera@usm.edu.ph>
2025-08-13 08:45:15 +0000

    update version 1.0.22

--- workbox-config.js
+++ workbox-config.js
@@ -2,4 +2,5 @@ module.exports = {
   globDirectory: './',
   globPatterns: ['**/*.{html,js,css,png,jpg,jpeg,mjs,ico,ftl,icc,bcmap,pfb,ttf,json,svg,pdf,PDF}'],
   swDest: './sw.js',
+  maximumFileSizeToCacheInBytes: 120 * 1024 * 1024 // 120MB
 };
